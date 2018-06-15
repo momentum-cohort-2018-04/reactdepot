ce3997cedf6be15d0316be02e0f95e1e2d186d42
Clinton Dreisbach <clinton@dreisbach.us>
2018-06-14 22:50:56 -0400

    Added GitHub authentication

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,37 @@ import './App.css'
 import CategoryListContainer from './CategoryListContainer'
 import CategoryPageContainer from './CategoryPageContainer'
 import PageHeader from './PageHeader'
+import firebase from '../firebase'
+import UserContext from '../UserContext'
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      user: firebase.auth().currentUser
+    }
+  }
+
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        user: user
+      })
+    })
+  }
+
   render () {
     return (
-      <div className='App'>
-        <Container>
-          <PageHeader />
-          <Route exact path='/' component={CategoryListContainer} />
-          <Route path='/category/:categoryId' component={CategoryPageContainer} />
-        </Container>
-      </div>
+      <UserContext.Provider value={this.state.user}>
+        <div className='App'>
+          <Container>
+            <PageHeader />
+            <Route exact path='/' component={CategoryListContainer} />
+            <Route path='/category/:categoryId' component={CategoryPageContainer} />
+          </Container>
+        </div>
+      </UserContext.Provider>
     )
   }
 }
--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import logo from '../logo.svg'
 import { Link } from 'react-router-dom'
-import { Level, LevelLeft, LevelRight, LevelItem, Title } from 'bloomer'
+import { Level, LevelLeft, LevelRight, LevelItem, Title, Button } from 'bloomer'
+import firebase from '../firebase'
+import UserContext from '../UserContext'
+const provider = new firebase.auth.GithubAuthProvider()
 
 class PageHeader extends React.Component {
+  handleLogin = (event) => {
+    event.preventDefault()
+    firebase.auth().signInWithRedirect(provider)
+  }
+
   render () {
+    const { user } = this.props
+
     return (<div className='PageHeader'>
       <Level>
         <LevelLeft>
@@ -20,11 +30,19 @@ class PageHeader extends React.Component {
           </LevelItem>
         </LevelLeft>
         <LevelRight>
-          <LevelItem>have a nice day</LevelItem>
+          <LevelItem>
+            {user
+              ? <span>Hello, {user.displayName}</span>
+              : <Button isColor='primary' onClick={this.handleLogin}>Login with GitHub</Button>}
+          </LevelItem>
         </LevelRight>
       </Level>
     </div>)
   }
 }
 
-export default PageHeader
+export default props => (
+  <UserContext.Consumer>
+    {user => <PageHeader {...props} user={user} />}
+  </UserContext.Consumer>
+)
